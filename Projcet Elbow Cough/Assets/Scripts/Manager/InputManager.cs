8e426f7d7ef04f4419b9acb35140dbc1e5d218cb
Bazzan <sebake01@gmail.com>
2020-10-30 17:27:50 +0100

    Init

---
+++ Projcet Elbow Cough/Assets/Scripts/Manager/InputManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static PlayerInputAction inputActions;
+
+
+    public static Vector2 mouseDirection;
+    public static Vector2 WasdInput;
+
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+
+
+
+
+}
